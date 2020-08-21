dbc3e3b946143c18eabfe02ee312b2de1b3530be
guycode <richlanedesign@gmail.com>
2020-08-21 07:51:12 -0600

    no changes/update gh-pages

--- build/js/components/Articles/Cards.jsx
+++ build/js/components/Articles/Cards.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import Articles from './CardSetup';
 
 export default class Cards extends React.Component {
-
 	render() {
 		return(
-			
 			<div className="cards">
 				<h2>Products</h2>
 					<div className="column">
